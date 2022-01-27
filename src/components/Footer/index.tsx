b9ab07aa615fc045fb04c0569f35e4f2f8cafaae
Ringo Kim <ringo@stamper.network>
2022-01-27 11:28:57 +0900

    Remove akash logo

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,8 +6,8 @@ import { ReactComponent as AkashLogo } from 'images/akash-logo.svg';
 const Footer: React.FC = () => {
   return (
     <div className="footer">
-      <AkashLogo/>
-      &nbsp;&nbsp;Deployed on Akash.&nbsp;
+      {/* <AkashLogo/>
+      &nbsp;&nbsp;Deployed on Akash.&nbsp; */}
       © 2022 powered by Cosmostation.
     </div>
   );
