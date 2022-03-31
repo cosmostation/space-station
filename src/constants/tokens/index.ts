a2a2a01f969124074543b18c94d4eac6c60c446d
Ringo Kim <ringo@stamper.network>
2022-03-31 17:11:00 +0900

    Add Gravity Bridge to Cosmos token info

--- src/constants/tokens/index.ts
+++ src/constants/tokens/index.ts
@@ -4,6 +4,7 @@ import gravityBridgeToCheqdTokens from 'constants/tokens/gb-cheqd-tokens.js';
 import gravityBridgeToEthTokens from 'constants/tokens/gb-eth-tokens';
 import gravityBridgeToIrisTokens from 'constants/tokens/gb-iris-tokens.json';
 import gravityBridgeToOsmosisTokens from 'constants/tokens/gb-osmosis-tokens.json';
+import gravityBridgeToCosmosTokens from 'constants/tokens/gb-cosmos-tokens.json';
 import gravityBridgeToStargazeTokens from 'constants/tokens/gb-stargaze-tokens.json';
 import irisToGravityBridgeTokens from 'constants/tokens/iris-gb-tokens.json';
 import osmosisToGravityBridgeTokens from 'constants/tokens/osmosis-gb-tokens.json';
@@ -23,6 +24,7 @@ export const ibcTokenFromToMap: Record<SupportedChain, { [key in SupportedChain]
   [SupportedChain.GravityBridge]: {
     [SupportedChain.Eth]: gravityBridgeToEthTokens,
     [SupportedChain.Osmosis]: gravityBridgeToOsmosisTokens,
+    [SupportedChain.Cosmos]: gravityBridgeToCosmosTokens,
     [SupportedChain.Stargaze]: gravityBridgeToStargazeTokens,
     [SupportedChain.Cheqd]: gravityBridgeToCheqdTokens,
     [SupportedChain.Iris]: gravityBridgeToIrisTokens
