c105abca050458c4328e3f69dd05aa953caef26a
Ringo Kim <ringo@stamper.network>
2022-04-18 20:59:35 +0900

    Fix huahua ibc denom

--- src/constants/tokens/gb-chihuahua-tokens.js
+++ src/constants/tokens/gb-chihuahua-tokens.js
@@ -1,7 +1,7 @@
 export default {
-  'ibc/3D5109BC236E6BD75F90CC03A5B7AA360FF3CB3F535B662227CEE78E83477AD9': {
+  'ibc/048BE20AE2E6BFD4142C547E04F17E5F94363003A12B7B6C084E08101BFCF7D1': {
     name: 'HUAHUA',
-    denom: 'ibc/3D5109BC236E6BD75F90CC03A5B7AA360FF3CB3F535B662227CEE78E83477AD9',
+    denom: 'ibc/048BE20AE2E6BFD4142C547E04F17E5F94363003A12B7B6C084E08101BFCF7D1',
     symbol: 'HUAHUA',
     decimals: 6,
     logoURI: 'https://raw.githubusercontent.com/cosmostation/cosmostation_token_resource/master/coin_image/tokens/token-chihuahua.svg'
